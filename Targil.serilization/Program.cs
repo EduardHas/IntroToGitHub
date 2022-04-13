a17cbf23c5e1286b80defba2c478ba254ac47ae1
EduardHas <93647839+EduardHas@users.noreply.github.com>
2022-04-13 03:47:53 +0300

    Serialization from 03-04-22

---
+++ Targil.serilization/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Targil.serilization
+{
+
+    /*                                                        תרגיל בסריאליזיישן
+                                                 1. כתבו מחלקה שיש בה 5 שדות properties
+                                                 2. צרו מופע של המחלקה עם ערכים
+                                                 3. בצעו תהליך של סרליזציה ושמרו בקובץ.
+                                             4. יש לכתוב פונקציה שטוענת קובץ ומבצעת תהליך דה-סרליזציה ויוצרת מופע של המחלקה
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
